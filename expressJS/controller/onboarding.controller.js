33cb7534d4f43f0e8f18199171ceaa5d6cc793b8
Adefokun Adeoluwa Israel <adefokunadeoluwaisrael@gmail.com>
2026-07-22 11:47:12 +0100

    express http

--- expressJS/controller/onboarding.controller.js
+++ expressJS/controller/onboarding.controller.js
@@ -24,4 +24,37 @@ function Register(req, res){
 }
 
 
-module.exports = Register;
+async function getUsers(req, res) {
+    try{
+        const allUser = await User.find();
+        res.status(200).json({message: "all user fetched successfully", allUser})
+    }catch(error){
+               res.status(500).json({message: "Internal Server Error", error})
+    }
+}
+
+async function updateUser(req, res){
+    try{
+            const {id} = req.params;
+            const {name, phone} = req.body;
+
+            if (!id){
+                res.status(400).send("User Id is required")
+            }
+
+            const updateuser = await User.findByIdAndUpdate(id, {name, phone}, { new: true });
+
+            if(!updateuser){
+                res.status(404).send("User NoT FounD")
+            }
+
+            res.status(200).json({message: "User updated succesfully", updateuser})
+
+    }catch(error){
+               res.status(500).json({message: "Internal Server Error", error})
+        
+    }
+}
+
+
+module.exports = {Register, getUsers, updateUser};
--- expressJS/routes/user.route.js
+++ expressJS/routes/user.route.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const getUser = require("../controller/user")
-const Register = require("../controller/onboarding.controller")
+// const getUser = require("../controller/user")
+const {Register, getUsers, updateUser} = require("../controller/onboarding.controller")
 const router = express();
 
-router.get('/users', getUser);
+router.get('/users', getUsers);
 router.post('/register', Register)
-
+router.put("/user/:id", updateUser)
 module.exports = router;
